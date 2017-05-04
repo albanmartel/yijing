fbd65d44de4d665580911bc90dbc82b48af5d66f
Alban Martel <albanmartel.developpeur@gmail.com>
2017-05-04 10:17:55 +0200

    maj du package.json pour permettre un npm start

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # yijing
 Vous avez une question, le yijing vous apporte un éclairage sur la situation et les évolutions possibles de la demande
+
+
+## Installation
+
+Install the yijing utility via [npm](http://npmjs.org/):
+
+```
+$ npm install -g yijing
+```
+
+## Command-line example
---
+++ app.js
@@ -0,0 +1,48 @@
+var yijing = require('./lib/yijing')
+
+var questionTirage = null
+var hexagrammePresentMessageMutations = ""
+var hexagrammeMuteMessageMutations= ""
+var hexagrammePresent = null
+var hexagrammeMute = null
+var hexagrammePresentPosition6et9 = null
+var hexagrammeMutePosition6et9 = null
+var getTirageNumPresent = yijing.getTirageNumPresent
+var tirage = yijing.tirage
+var mutation = yijing.mutation
+var afficherOracle = yijing.afficherOracle
+var afficherMessagesMutations = yijing.afficherMessagesMutations
+var getHexagrammePresent = yijing.getHexagrammePresent
+var getHexagrammeMute = yijing.getHexagrammeMute
+var getHexagrammePresentPosition6et9 = yijing.getHexagrammePresentPosition6et9
+var getHexagrammeMutePosition6et9 = yijing.getHexagrammeMutePosition6et9
+const oracle = yijing.getOracle()
+const probabiliteRealisation = yijing.getProbabiliteRealisation()
+const binaryToHexagramme = yijing.getBinaryToHexagramme()
+const menuConsole = yijing.menuConsole
+const afficherConsole = yijing.afficherConsole
+const definirCodeInterpretation = yijing.definirCodeInterpretation
+const setQuestionTirage = yijing.setQuestionTirage
+const getQuestionTirage = yijing.getQuestionTirage
+
+/* Appel en cascade des fonctions asynchrones par des appels en callback */
+function main(){
+  menuConsole()
+  tirage6fois()
+}
+
+function tirage6fois(){
+  for (i=0 ; i<6 ; i++){
+    tirage()
+  }
+
+  excMutation()
+}
+
+function excMutation(){
+  mutation()
+  afficherConsole()
+}
+
+/* Appel du point d'entrée */
+main()
